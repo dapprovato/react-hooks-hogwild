41faee8c49095ab45ae71e1114eaa3dd4178ffaf
Daria Approvato <dariaapprovato@gmail.com>
2022-12-08 13:20:03 -0500

    commit tree 1a7ca5d1913157bc00dc80e3267daa17ff2f2f1c

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
 
 function App() {
+	const [pigInfo, showPigInfo] = useState(false)
+
+	function handlePigInfo() {
+		showPigInfo(!pigInfo)
+	}
+
 	return (
 		<div className="App">
 			<Nav />
+			{hogs.map((hog) =>
+				<div>
+					<div className="pigTile">{hog.name}</div>
+					<button className="pigTile" onClick={handlePigInfo}>Specialty: {hog.specialty} Weight: {hog.weight}</button>
+					<img src={hog.image} />
+				</div>
+			)}
+			{/* <button className="pigTile" onClick={handlePigInfo}>{pigInfo ? hogs.name : "pigTileInfo"}</button> */}
 		</div>
 	);
 }
